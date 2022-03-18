6db4cf190f36dd23895105e0183e151aa31a64fb
Byron Gomez Jr <byrongomezjr@protonmail.com>
2022-03-18 14:04:54 -0400

    added custom pages.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Home from './pages/home/Home.jsx';
 import TopBar from './components/topbar/TopBar.jsx';
-//import Coding from './pages/coding/Coding.jsx';
-//import Blog from './pages/blog/Blog.jsx';
+import Coding from './pages/coding/Coding.jsx';
+import Blog from './pages/blog/Blog.jsx';
 
 /* eslint-disable react/jsx-no-undef */
 function App() {
@@ -9,6 +9,8 @@ function App() {
     <>
     <TopBar/>
     <Home/>
+    <Blog/>
+    <Coding/>
     </>
   );
 }
