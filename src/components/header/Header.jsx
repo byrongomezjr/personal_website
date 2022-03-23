7319ccb6b11bfc068e63433514043c0ce906b346
Byron Gomez Jr <byrongomezjr@protonmail.com>
2022-03-23 16:47:50 -0400

    better responsiveness.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,15 +3,17 @@ import './header.css';
 export default function Header() {
   return (
     <div className="header">
-      <section className="headerTitle">
-        <section className="headerTitleHello">
-          Hello, I'm Byron.
-        </section>
-    </section>
-    <section className="photoIcon">
-        <img style={{width: 200, height: 200, borderRadius: 200/2}} 
-    src="https://avatars.githubusercontent.com/u/60487113?v=4" alt="" />
-    </section>
+
+    <container className="mainContainer">
+          
+          <section className="photoIcon">
+          <h1 className="hello">Hello, I'm Byron.</h1>
+          
+          <img style={{width: 200, height: 200, borderRadius: 200/2}} 
+          src="https://avatars.githubusercontent.com/u/60487113?v=4" alt="" />
+          </section>
+    </container>
+    
     </div>
   )
 }
