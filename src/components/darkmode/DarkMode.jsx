1fd9480ca497be7b1d074c1c16002a26f9951012
Byron Gomez Jr <byrongomezjr@protonmail.com>
2022-04-03 16:32:49 -0400

    added preferred user settings for dark mode or light mode on their browser.

--- src/components/darkmode/DarkMode.jsx
+++ src/components/darkmode/DarkMode.jsx
@@ -1,15 +1,30 @@
 import './DarkMode.css';
 
+//DARK MODE
 const setDark = () => {
   localStorage.setItem('theme', 'dark');
   document.documentElement.setAttribute('data-theme', 'dark');
 };
 
+//LIGHT MODE
 const setLight = () => {
   localStorage.setItem('theme', 'light');
   document.documentElement.setAttribute('data-theme', 'light');
 };
 
+//Preferred user settings for dark mode or light mode.
+const storedTheme = localStorage.getItem("theme");
+
+const prefersDark = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+const defaultDark = storedTheme === "dark" || (storedTheme === null && prefersDark);
+
+if (defaultDark) {
+  setDark();
+}
+
+
+//TOGGLE FROM LIGHT TO DARK
 const toggleTheme: changeEventHandler<HTMLInputElement> = (e) => {
   if (e.target.checked) {
     setDark();
@@ -23,7 +38,8 @@ const DarkMode = () => {
     <div className='toggle-theme-wrapper'>
       <span></span>
       <label className='toggle-theme' htmlFor='checkbox'>
-        <input type='checkbox' id='checkbox' onChange={toggleTheme} />
+        <input type='checkbox' id='checkbox' onChange={toggleTheme} 
+        defaultChecked={defaultDark} />
 
         <span className='slider round'></span>
       </label>
